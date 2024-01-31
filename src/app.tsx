96e638e0cdc08ed261331f6fde81dd57fd0ba583
ComboRush <comborush.init@proton.me>
2024-01-31 09:47:44 +0200

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,18 @@
 // @refresh reload
-import { createSignal } from "solid-js";
+import { Motion } from "solid-motionone";
+import Bubble from "./components/bubble";
 import "./app.css";
+import Wrapper from "./components/wrapper";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <main class="w-full h-full">
+      <Motion.div class="fixed flex bottom-24 w-full z-10" animate={{ opacity: 1, bottom: "6rem" }} initial={{ opacity: 0, bottom: 0 }} transition={{ duration: 1 }}>
+        <Wrapper>
+          <Bubble>
+          </Bubble>
+        </Wrapper>
+      </Motion.div>
     </main>
   );
 }
