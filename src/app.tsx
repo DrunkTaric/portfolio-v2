f026eaba2815d0e22b5304d48180307a65029efc
ComboRush <comborush.init@proton.me>
2024-02-06 10:24:38 +0200

    porgamming languages skills

--- src/app.tsx
+++ src/app.tsx
@@ -10,6 +10,12 @@ export default function App() {
       <Motion.div class="fixed flex bottom-24 w-full z-10" animate={{ opacity: 1, bottom: "6rem" }} initial={{ opacity: 0, bottom: 0 }} transition={{ duration: 1 }}>
         <Wrapper>
           <Bubble>
+          <Bubble border style="col-span-3 space-y-2">
+            <Progress size="md" precentage={100} name="Lua"></Progress>
+            <Progress size="md" precentage={100} name="Javascript"></Progress>
+            <Progress size="md" precentage={85} name="Python"></Progress>
+            <Progress size="md" precentage={75} name="C#"></Progress>
+          </Bubble>
           </Bubble>
         </Wrapper>
       </Motion.div>
