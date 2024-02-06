d29966e374ce25d0a843cdf75a0a674eb600e67b
ComboRush <comborush.init@proton.me>
2024-02-06 10:25:55 +0200

    removing motion fromt he main thing

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,6 @@ import Wrapper from "./components/wrapper";
 export default function App() {
   return (
     <main class="w-full h-full">
-      <Motion.div class="fixed flex bottom-24 w-full z-10" animate={{ opacity: 1, bottom: "6rem" }} initial={{ opacity: 0, bottom: 0 }} transition={{ duration: 1 }}>
         <Wrapper>
           <Bubble>
           <Bubble style="w-full h-full col-span-3 row-span-3 max-h-[500px] rounded-2xl">
@@ -22,7 +21,6 @@ export default function App() {
           </Bubble>
           </Bubble>
         </Wrapper>
-      </Motion.div>
     </main>
   );
 }
