c0946e7cab5145ffaf9ffb2d9e7592a9db56c022
ComboRush <comborush.init@proton.me>
2024-02-06 10:24:20 +0200

    wrapper for the grid

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,6 +1,6 @@
 export default function(props: { children: any }) {
     return (
-        <div class="m-auto">
+        <div class="w-full h-full grid grid-cols-10 gap-5 auto-rows-max">
             { props.children }
         </div>
     )
