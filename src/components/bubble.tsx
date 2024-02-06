dcdb86d7e472847c256b3a345ace495863b6be93
ComboRush <comborush.init@proton.me>
2024-02-06 10:23:56 +0200

    making the bubble with an optional border

--- src/components/bubble.tsx
+++ src/components/bubble.tsx
@@ -1,9 +1,13 @@
 import { clsx } from "clsx";
 
-export default function Bubble(props: { children?: any, style?: string }) {
+export default function Bubble(props: { children?: any, style?: string , border?: boolean}) {
     return (
         <div 
-            class={clsx("w-auto h-auto p-3 pl-5 pr-5 lg:p-5 lg:pl-7 lg:pr-7 rounded-xl bg-white/10", props.style)}
+            class={clsx(
+                "w-auto h-auto bg-white/10", 
+                props.style,
+                props.border? "p-3 pl-5 pr-5 lg:p-5 lg:pl-7 lg:pr-7 rounded-xl" : null
+            )}
         >{ props.children }</div>
     );
 }
