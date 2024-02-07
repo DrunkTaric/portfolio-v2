8e4d2c413e2fcbfabd08432d9242907e61d44493
ComboRush <comborush.init@proton.me>
2024-02-07 21:13:34 +0200

    making the bubble with animation

--- src/components/bubble.tsx
+++ src/components/bubble.tsx
@@ -1,13 +1,17 @@
 import { clsx } from "clsx";
+import { Motion } from "solid-motionone";
 
 export default function Bubble(props: { children?: any, style?: string , border?: boolean}) {
     return (
-        <div 
+        <Motion.div 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 2}}
             class={clsx(
                 "w-auto h-auto bg-white/10", 
                 props.style,
                 props.border? "p-3 pl-5 pr-5 lg:p-5 lg:pl-7 lg:pr-7 rounded-xl" : null
             )}
-        >{ props.children }</div>
+        >{ props.children }</Motion.div>
     );
 }
