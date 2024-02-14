cc5d569e81203a44e0da8b611a88594ce9955bae
ComboRush <comborush.init@proton.me>
2024-02-14 22:51:18 +0200

    image loader

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx"
+import Bubble from "./bubble"
 import { createSignal } from "solid-js"
 
 export default function Image(props: { src: string, class?: string }) {
     const [Loading, setLoading] = createSignal<boolean>(true)
     return (
         <div>
-            { Loading() && <h1 class={clsx("text-white", props.class)}>Loading..</h1> }
+            { Loading() && <Bubble style={clsx("text-white", props.class, "animate-pulse")}></Bubble> }
             <img class={clsx(props.class, Loading()? "hidden": null)} src={props.src} onLoad={() => setLoading(false)}></img>
         </div>
     )
