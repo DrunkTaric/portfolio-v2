a992f9edc5005e6fae551c882d4bb7e25431f506
DrunkTaric <comborush.init@proton.me>
2024-03-25 23:00:36 +0200

    making a circle that moves with the cursor

--- src/app.tsx
+++ src/app.tsx
@@ -9,9 +9,32 @@ import Social from "./components/social";
 import "./app.css";
 
 export default function App() {
-  return (
-    <main class="w-screen h-screen">
+  const [Pos, setPos] = createSignal({x: 0, y: 0})
+
+  createEffect(() => {
+    document.addEventListener("mousemove", function(event) {
+      if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+      }
       
+      var cursor = document.getElementById("cursor")
+
+      cursor.animate({
+            left: `${event.pageX - 120}px`,
+            top: `${event.pageY - 120}px`
+      }, {duration: 1000, fill: "forwards"});
+    })
+  }, 0)
+
     </main>
   );
 }
