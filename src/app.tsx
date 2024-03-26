47b606074d0c22e4d4c40c99c7c237c9912c050e
ComboRush <comborush.init@proton.me>
2024-03-27 01:37:39 +0200

    removing typescript errors

--- src/app.tsx
+++ src/app.tsx
@@ -18,23 +18,23 @@ export default function App() {
   createEffect(() => {
     document.addEventListener("mousemove", function(event) {
       if (event.pageX == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
+        let eventDoc = (event.target && (event.target as any).ownerDocument) || document;
+        let doc = eventDoc.documentElement;
+        let body = eventDoc.body;
 
-            event.pageX = event.clientX +
-              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-              (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        (event as any).pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+          (event as any).pageY = event.clientY +
+          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+          (doc && doc.clientTop  || body && body.clientTop  || 0 );
       }
       
       var cursor = document.getElementById("cursor")
 
-      cursor.animate({
-            left: `${event.pageX - 120}px`,
-            top: `${event.pageY - 120}px`
+      cursor?.animate({
+        left: `${event.pageX - 120}px`,
+        top: `${event.pageY - 120}px`
       }, {duration: 1000, fill: "forwards"});
     })
   }, 0)
