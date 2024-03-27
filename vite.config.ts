f6b9c8ee48545b7be7515930e3f647ad9204d28e
ComboRush <comborush.init@proton.me>
2024-03-27 03:01:00 +0200

    rebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "@solidjs/start/config";
-
-import solid from "solid-start/vite";
-import vercel from "solid-start-vercel";
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })]
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
 });
