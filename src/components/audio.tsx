b543d854694302dc82fd99926a7694dab5d122dd
ComboRush <comborush.init@proton.me>
2024-03-27 03:58:25 +0200

    making the audio work again

--- src/components/audio.tsx
+++ src/components/audio.tsx
@@ -1,27 +1,28 @@
-import { FaSolidVolumeHigh, FaSolidVolumeXmark } from 'solid-icons/fa'
-import { createSignal, createEffect } from "solid-js"
+import { FaSolidVolumeHigh, FaSolidVolumeXmark } from 'solid-icons/fa';
+import { createSignal, createEffect } from "solid-js";
+import { makeAudio } from '@solid-primitives/audio';
+import song from "../assets/song.mp3"
 
 export default function Audio() {
   
-  let interval;
+  let interval: number | null;
   const [Muted, setMuted] = createSignal(true)
+  const audio = makeAudio(song);
 
   function Sound() {
     setMuted(!Muted())
-    const audio = document.getElementById("sound")
-    audio.autoplay = true
-    audio.muted = Muted()
-    const ball = document.getElementById("ball")
-    const bar = document.getElementById("bar")
-    if (!interval) {
+
+    if (audio.paused) {
+      audio.play()
+      const ball = document.getElementById("ball")
       interval = setInterval(() => {
-        const audio = document.getElementById("sound") 
-        const ball = document.getElementById("ball") 
-        const precentage_time = (audio.currentTime/audio.duration) * 100
-        const bar_length = (299.083 * precentage_time) / 100
-        ball.style.height = `${bar_length}px`
+        const precentage_time = (audio.currentTime / audio.duration) * 100
+        const bar_length = (299.083 * precentage_time) / 100;
+        (ball as HTMLDivElement).style.height = `${bar_length}px`
       }, 100)
     }
+
+    audio.muted = Muted();
   }
   
   return (
@@ -32,9 +33,6 @@ export default function Audio() {
       </button>
       <div id="bar" class="flex ml-auto mr-auto bg-gray-300 bg-opacity-10 rounded-full h-[100%] w-4 overflow-visible">
         <div id="ball" class="bg-white w-4 h-0 mb-0 mt-auto rounded-full">
-          <audio id="sound">
-            <source src="song.mp3" type="audio/mpeg"/>
-          </audio>   
         </div>
       </div>
     </div>
