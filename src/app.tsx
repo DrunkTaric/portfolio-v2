46eed23f8cf9e883ed70afc595805b4f7935a35e
ComboRush <comborush.init@proton.me>
2024-03-27 16:06:35 +0200

    making the code smaller

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,20 @@
 // @refresh reload
 import Image from "./components/image";
 import Audio from "./components/audio";
+import Cursor from "./components/cursor";
 import Bubble from "./components/bubble";
 import { ImLinkedin2 } from 'solid-icons/im'
 import Progress from "./components/progress";
 import Projects from "./components/projects";
 import ProfilePic from "./assets/profile-pic.jpg";
-import { createEffect, createSignal } from "solid-js";
 import { SiNeovim, SiGit, SiNodedotjs, SiLinux, SiSpotify, SiDiscord, SiGithub } from 'solid-icons/si';
 
 export default function App() {
-  const [Pos, setPos] = createSignal({x: 0, y: 0})
-
-  createEffect(() => {
-    document.addEventListener("mousemove", function(event) {
-      if (event.pageX == null && event.clientX != null) {
-        let eventDoc = (event.target && (event.target as any).ownerDocument) || document;
-        let doc = eventDoc.documentElement;
-        let body = eventDoc.body;
-
-        (event as any).pageX = event.clientX +
-          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-          (doc && doc.clientLeft || body && body.clientLeft || 0);
-          (event as any).pageY = event.clientY +
-          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-          (doc && doc.clientTop  || body && body.clientTop  || 0 );
-      }
-      
-      var cursor = document.getElementById("cursor")
-
-      cursor?.animate({
-        left: `${event.pageX - 120}px`,
-        top: `${event.pageY - 120}px`
-      }, {duration: 1000, fill: "forwards"});
-    })
-  }, 0)
-
   return (
     <main class="flex flex-col w-screen h-screen justify-center items-center">
-      <div id="cursor" class="w-[15rem] h-[15rem] bg-purple-800/50 blur-2xl absolute rounded-full -z-50"></div>
+
+      <Cursor/>
+
       <h1 class="text-7xl text-white p-5 font-bold">DrunkTaric</h1>
       <section class="w-[85%] h-[55%] grid grid-cols-12 grid-rows-5 gap-5">
         <Image src={ProfilePic} class="h-full w-full col-span-4 row-span-5 select-none"/>
@@ -84,10 +60,11 @@ export default function App() {
           <Progress precentage={70} name={"C#"} size="md" />
         </Bubble>
       </section>
+
       {false && <h1 class="text-5xl text-white p-5 font-bold">Projects</h1>}
       {false && <Projects/> }
       {false && <h1 class="text-5xl text-white p-5 font-bold">Roadmap</h1> }
-      
+
     </main>
   );
 }
