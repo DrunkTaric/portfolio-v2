ab10209ce03aba89fa3f852edeaaa1d91c3963ca
ComboRush <comborush.init@proton.me>
2024-03-28 21:25:45 +0200

    things

--- src/app.tsx
+++ src/app.tsx
@@ -9,22 +9,24 @@ import Projects from "./components/projects";
 import { isMobile } from 'mobile-device-detect';
 import ProfilePic from "./assets/profile-pic.jpg";
 import { SiNeovim, SiGit, SiNodedotjs, SiLinux, SiSpotify, SiDiscord, SiGithub } from 'solid-icons/si';
-
 export default function App() {
+
   return (
-    <main class="flex flex-col w-screen h-screen justify-center items-center">
+    <main class="flex flex-col w-screen h-screen">
 
       {!isMobile && <Cursor/>}
 
       <h1 class="text-7xl text-white p-5 font-bold">DrunkTaric</h1>
-      <section class="w-[85%] h-[55%] grid grid-cols-12 grid-rows-5 gap-5">
-        <Image src={ProfilePic} class="h-full w-full col-span-4 row-span-5 select-none"/>
+      <Image src={ProfilePic} class="h-full w-full col-span-4 row-span-5 select-none"/>
+      
+      <section class=" m-auto w-fit h-[50rem] grid grid-rows-12 auto-rows-fr p-5 sm:p-10 xl:grid-cols-12 xl:grid-rows-5 gap-5">
+        <Image src={ProfilePic} class="h-full w-full row-span-6 xl:col-span-4 xl:row-span-5 select-none"/>
 
-        <Bubble style="row-span-2 col-span-6 text-2xl" border>
+        <Bubble style="xl:row-span-2 xl:col-span-6 text-2xl" border>
           Passionate developer with 7 years of coding experience, driven by a love for programming, technology, and AI. Actively seeking new professional opportunities. Dedicated to quality, with a strong focus on problem-solving. Enjoys coding, reading, and consistently seeks to enhance skills within the tech industry.
         </Bubble>
 
-        <Bubble style="row-span-5 grid grid-rows-5" border>
+        <Bubble style="xl:row-span-5 grid grid-rows-5" border>
           <SiLinux class="w-full h-full m-auto" />
           <SiSpotify class="w-full h-full m-auto" />
           <SiGit class="w-full h-full m-auto" />
@@ -32,29 +34,29 @@ export default function App() {
           <SiNodedotjs class="w-full h-full m-auto" />
         </Bubble>
 
-        <Bubble style="flex flex-col space-y-4 row-span-5" border>
+        <Bubble style="flex flex-col space-y-4 xl:row-span-5" border>
           <Audio/>
         </Bubble>
 
-        <Bubble style="flex col-span-2 rounded-[1.5rem]">
+        <Bubble style="flex xl:col-span-2 rounded-[1.5rem]">
           <a href="https://www.linkedin.com/in/comborush/" target="_blank" class="m-auto">
             <ImLinkedin2 class="w-12 h-12"/>
           </a>
         </Bubble>
 
-        <Bubble style="flex col-span-2 rounded-[1.5rem]">
+        <Bubble style="flex xl:col-span-2 rounded-[1.5rem]">
           <a href="https://github.com/DrunkTaric" target="_blank" class="m-auto">
             <SiGithub class="w-12 h-12"/>
           </a>
         </Bubble>
 
-        <Bubble style="flex col-span-2 rounded-[1.5rem]">
+        <Bubble style="flex xl:col-span-2 rounded-[1.5rem]">
           <a href="https://discordapp.com/users/921584553640992810" target="_blank" class="m-auto">
             <SiDiscord class="w-14 h-14"/>
           </a>
         </Bubble>
 
-        <Bubble style="grid grid-rows-4 row-span-2 col-span-6" border>
+        <Bubble style="grid grid-rows-4 xl:row-span-2 xl:col-span-6" border>
           <Progress precentage={100} name={"Lua"} size="md" />
           <Progress precentage={95} name={"Javascript"} size="md" />
           <Progress precentage={80} name={"Python"} size="md" />
